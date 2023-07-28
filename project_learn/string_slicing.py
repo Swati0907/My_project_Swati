e1a6dea13ca7251e05158926a307caacebc3b518
swati0907 <swati.premsudha@gmail.com>
2023-07-28 22:56:13 +0530

    adding string slicing file

---
+++ project_learn/string_slicing.py
@@ -0,0 +1,12 @@
+def main():
+    my_string = "Hello world"
+    my_string1 = "How Are You"
+    my_string2 = "I am good"
+
+    print("Slicing of string", my_string[:3])
+    print("Slicing of string", my_string1[3:])
+    print("Slicing of string", my_string2[5:])
+
+
+if __name__ == '__main__':
+    main()
