6215f64bbe0f03fb8811355665c19468fd152de0
swati0907 <swati.premsudha@gmail.com>
2023-07-28 23:12:09 +0530

    adding string slicing file

---
+++ project_learn/reversing_a_string.py
@@ -0,0 +1,10 @@
+def main():
+    my_string = "12348787"
+    print("Reversing string", my_string[::-1])
+
+    # start stop and step(Start point is at index 2 stop is at 7 and the step size will 1)
+    print("dj", my_string[2:7:1])
+
+
+if __name__ == '__main__':
+    main()
