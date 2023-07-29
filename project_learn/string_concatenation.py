61fb6cf83994eed004b06ce146fcc32b11bb40b0
swati0907 <swati.premsudha@gmail.com>
2023-07-29 11:00:48 +0530

    adding string part

---
+++ project_learn/string_concatenation.py
@@ -0,0 +1,9 @@
+def main():
+    string1 = "Hello World "
+    string2 = "is beautiful"
+    world = string1[6:]
+    print(world + string2)
+    
+
+if __name__ == '__main__':
+    main()
---
+++ project_learn/string_methods.py
@@ -0,0 +1,10 @@
+def main():
+    string = "Hello World"
+
+    print(string.upper())
+    print(string.lower())
+    print(string.split())
+
+
+if __name__ == '__main__':
+    main()
